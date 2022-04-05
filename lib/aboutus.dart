bebbb11fdb6c3080d9ad23461b9d4ea970cbf27a
Sayan Banerjee <banerjeesayan09082002@gmail.com>
2022-04-05 20:26:08 +0530

    About Us

--- lib/aboutus.dart
+++ lib/aboutus.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class AboutUs extends StatefulWidget {
   @override
@@ -49,7 +50,7 @@ class _AboutUs extends State<AboutUs> {
                 ),
                 Tab(
                     icon: Icon(
-                  Icons.circle,
+                  FontAwesomeIcons.globe,
                 )),
               ],
             ),
